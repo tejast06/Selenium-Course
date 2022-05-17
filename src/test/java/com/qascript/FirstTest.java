0c55cca0fd75ff4205e10f62219d6872cbcacdbe
Tejas <46339378+tejast06@users.noreply.github.com>
2022-05-17 17:32:39 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -18,8 +18,22 @@ public class FirstTest {
         ChromeOptions options = new ChromeOptions();
 	options.addArguments("--no-sandbox");
 	options.addArguments("window-size=1400,800");    
-        options.addArguments("headless");
+        options.addArguments("--headless");
         options.addArguments("disable-gpu");
+	    
+options.addArguments("--disable-dev-shm-usage"); 
+options.addArguments("--aggressive-cache-discard"); 
+options.addArguments("--disable-cache"); 
+options.addArguments("--disable-application-cache"); 
+options.addArguments("--disable-offline-load-stale-cache"); 
+options.addArguments("--disk-cache-size=0");
+options.addArguments("--dns-prefetch-disable"); 
+options.addArguments("--no-proxy-server"); 
+options.addArguments("--log-level=3"); 
+options.addArguments("--silent"); 
+options.addArguments("--disable-browser-side-navigation"); options.setPageLoadStrategy(PageLoadStrategy.NORMAL); 
+options.setProxy(null);
+	    
         driver = new ChromeDriver(options);
 	driver.manage().window().maximize();
         driver.get("https://www.impactguru.com");
