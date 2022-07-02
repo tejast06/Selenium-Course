9f56e52e6170b44b8369f5bb88effb0747514596
Tejas <46339378+tejast06@users.noreply.github.com>
2022-07-02 15:11:26 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -5,21 +5,18 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class FirstTest {
 
 
     @Test
-    public void OpenBrowser()  {
-        WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-        options.addArguments("disable-gpu");
-        driver = new ChromeDriver(options);
-        driver.get("https://www.google.com");
-        System.out.println("Title of the page is: " + driver.getTitle());
-        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
+       ChromeOptions options = new ChromeOptions();
+       DesiredCapabilities capabilities = new DesiredCapabilities();
+       capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+       System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+       ChromeDriver driver = new ChromeDriver(capabilities);
+       DriverFactory.changeWebDriver(driver)WebUI.navigateToUrl('http://www.google.com');
 	    
 	    
     }
