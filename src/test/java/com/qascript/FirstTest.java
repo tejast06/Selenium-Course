d5a6dfcc0fb895b67a33a5522b654046476a6fdb
Tejas <46339378+tejast06@users.noreply.github.com>
2022-07-02 14:51:50 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -12,33 +12,23 @@ public class FirstTest {
     @Test
     public void OpenBrowser()  {
      
-	 System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-	 WebDriver driver;  
-	    
-        ChromeOptions options = new ChromeOptions();
-	options.addArguments("--no-sandbox");
-	options.addArguments("window-size=1400,800");    
-        options.addArguments("--headless");
-        options.addArguments("disable-gpu");
-	    
-options.addArguments("--disable-dev-shm-usage"); 
-options.addArguments("--aggressive-cache-discard"); 
-options.addArguments("--disable-cache"); 
-options.addArguments("--disable-application-cache"); 
-options.addArguments("--disable-offline-load-stale-cache"); 
-options.addArguments("--disk-cache-size=0");
-options.addArguments("--dns-prefetch-disable"); 
-options.addArguments("--no-proxy-server"); 
-options.addArguments("--log-level=3"); 
-options.addArguments("--silent"); 
-options.addArguments("--disable-browser-side-navigation"); 
-	    //options.setPageLoadStrategy(PageLoadStrategy.NORMAL); 
-options.setProxy(null);
-	    
+	 WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");			
+	//options.addArguments("headless");
+        options.addArguments("browser");		
+	options.addArguments("start-maximized"); 
+        options.addArguments("enable-automation"); 
+        options.addArguments("--no-sandbox"); 
+        options.addArguments("--disable-infobars"); 
+        options.addArguments("--disable-dev-shm-usage"); 
+        options.addArguments("--disable-browser-side-navigation"); 
+        options.addArguments("--disable-gpu"); 
+        options.addArguments("--disable-features=VizDisplayCompositor"); 
+	ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NONE);
         driver = new ChromeDriver(options);
-	driver.manage().window().maximize();
         driver.get("https://www.impactguru.com");
-        System.out.println("Title of the page is: " + driver.getTitle());
+	System.out.println("Title of the page is: " + driver.getTitle());
         Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Crowdfunding in India: Best Fundraising Platform Online | ImpactGuru"));
 	    
 	    
