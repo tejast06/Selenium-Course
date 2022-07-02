3f39fe198477967a08bf782190f2f716f1f8ba22
Tejas <46339378+tejast06@users.noreply.github.com>
2022-07-02 14:57:19 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -11,27 +11,15 @@ public class FirstTest {
 
     @Test
     public void OpenBrowser()  {
-     
-	 WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");			
-	options.addArguments("headless");
-        options.addArguments("browser");		
-	options.addArguments("start-maximized"); 
-        options.addArguments("enable-automation"); 
-        options.addArguments("--no-sandbox"); 
-        options.addArguments("--disable-infobars"); 
-        options.addArguments("--disable-dev-shm-usage"); 
-        options.addArguments("--disable-browser-side-navigation"); 
-        options.addArguments("--disable-gpu"); 
-        options.addArguments("--disable-features=VizDisplayCompositor"); 
-	ChromeOptions options = new ChromeOptions();
-       // options.setPageLoadStrategy(PageLoadStrategy.NONE);
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("disable-gpu");
         driver = new ChromeDriver(options);
-        driver.get("https://www.impactguru.com");
-	System.out.println("Title of the page is: " + driver.getTitle());
-        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Crowdfunding in India: Best Fundraising Platform Online | ImpactGuru"));
-	    
-	    
+        driver.get("https://www.google.com");
+        System.out.println("Title of the page is: " + driver.getTitle());
+        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
 	    
 	    
     }
